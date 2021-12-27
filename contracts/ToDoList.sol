3ad926f6566764f24fad8085594d27dbde109b44
= <rabhi.aissam@hotmail.fr>
2021-12-27 18:07:46 +0100

    maj

--- contracts/ToDoList.sol
+++ contracts/ToDoList.sol
@@ -3,4 +3,22 @@ pragma solidity >0.5.16;
 
 contract ToDoList {
     uint public taskCount = 0;
+
+    struct Task {
+        uint id;
+        string content;
+        bool completed;
+    }
+
+    mapping(uint => Task) public tasks;
+
+    constructor() {
+        createTask("check out Sapienzauniversiy.com");
+    }
+
+    function createTask(string memory _content) public {
+        taskCount ++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
+
 }
